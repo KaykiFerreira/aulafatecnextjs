eb4d06f939cfb890949ae5eea26bffab13ee2af1
Kayki Ferreira <kaykiclashroyale30@gmail.com>
2024-05-20 19:44:36 +0000

    aula

--- my-app/src/app/userlogin/page.jsx
+++ my-app/src/app/userlogin/page.jsx
@@ -1,16 +1,20 @@
 import Link from 'next/link';
 import styles from './page.module.css' 
 
-export default function Login () {
+export default function(){
     return(
-        <div className={styles.container}> 
-        <h1>Login</h1>
-        <span>E-mail</span>
-        <input />
-        <span>Senha</span>
-        <input />
-        <br />
-        <p><Link href={'/'}>Voltar</Link></p>
-        </div>
+        <>
+           <div>
+               <h1>Login</h1>
+               <label for="email">E-mail</label>
+               <input type="email" id='email' name='email' requeired/>
+               <label for="password">Senha</label>
+               <input type="password" id='password' name='password' requeired/>
+               <button type="submit">Entrar</button>
+               <p>Não tem conta? <a href="#">Crie uma agora!</a></p>
+                <p><Link href={'/userlogin'}>Login</Link></p>
+                <p><Link href={'/products'}>Produtos</Link></p>
+           </div>
+        </>
     )
 }
