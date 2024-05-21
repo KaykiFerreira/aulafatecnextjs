a2d1eea5064f4bf0b61b5d7767518cbfc7795638
Kayki Ferreira <kaykiclashroyale30@gmail.com>
2024-05-21 13:56:30 +0000

    novo2

--- my-app/src/app/userlogin/page.jsx
+++ my-app/src/app/userlogin/page.jsx
@@ -5,29 +5,26 @@ export default function () {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.leftPane}></div>
-        <div className={styles.rightPane}>
-          <div className={styles.loginBox}>
-            <div className={styles.logo}></div>
-            <h2>Insira seus dados para entrar</h2>
-            <form>
-              <div className={styles.inputBox}>
-                <input type="email" required />
-                <label>E-mail</label>
-              </div>
-              <div className={styles.inputBox}>
-                <input type="password" required />
-                <label>Senha</label>
-              </div>
-              <div className={styles.button}>Entrar</div>
-            </form>
-            <div className={styles.linksContainer}>
-              <div className={styles.linksb}>
-                <a href="#">Esqueceu a senha? Redefina</a>
-              </div>
-              <div className={styles.links}>
-                <a href="#">Não tem conta? Crie uma agora</a>
-              </div>
+        <div className={styles.loginBox}>
+          <div className={styles.logo}></div>
+          <h2>Insira seus dados para entrar</h2>
+          <form>
+            <div className={styles.inputBox}>
+              <input type="email" required />
+              <label>E-mail</label>
+            </div>
+            <div className={styles.inputBox}>
+              <input type="password" required />
+              <label>Senha</label>
+            </div>
+            <div className={styles.button}>Entrar</div>
+          </form>
+          <div className={styles.linksContainer}>
+            <div className={styles.linksb}>
+              <a href="#">Esqueceu a senha? Redefina</a>
+            </div>
+            <div className={styles.links}>
+              <a href="#">Não tem conta? Crie uma agora</a>
             </div>
           </div>
         </div>
