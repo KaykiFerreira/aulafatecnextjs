c244d99d494768cf48e5d985d337deb618ea63cc
Kayki Ferreira <kaykiclashroyale30@gmail.com>
2024-05-23 01:06:38 +0000

    login2

--- my-app/src/app/userlogin/page.jsx
+++ my-app/src/app/userlogin/page.jsx
@@ -1,14 +1,44 @@
 import Link from 'next/link';
 import styles from './page.module.css'
+import Image from 'next/image';
 
 export default function () {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.loginBox}>
+        <div className={styles.left}></div>
+        <div className={styles.right}>
+          <div className={styles.loginBox}>
+            <div className={styles.logo}>
+              <img src="/Image/logo.svg" alt="Logo" srcset="" />
+            </div>
+            <h2>Insira seus dados para entrar</h2>
+              <div className={styles.inputbox}>
+              <label>E-mail</label>
+              <input type="email" required />
+              </div>
+              <div className={styles.inputbox}>
+              <label>Senha</label>
+              <input type="password" required />
+              </div>
+              <div className={styles.button}>Entrar</div>
+              <div className={styles.linksContainer}>
+          <div className={styles.link}>
+          Esqueceu a senha?<a href="#"> Redefina</a>
+            </div>
+            <div className={styles.links}>
+            Não tem conta?<a href="#"> Crie uma agora</a>
+            </div>
+        </div>
+          </div>
+         
+            
+          </div> 
+          
+        {/* <div className={styles.loginBox}>
           <div className={styles.img}></div>
-          <img src="" alt="" />
-          <h2>Insira seus dados para entrar</h2>
+          <Image src="Image/logo.svg" alt="" />
+           */}
             {/* <div className={styles.inputBox}>
               <label className={styles.inputBoxlabel}>E-mail</label>
               <input type="email" required />
@@ -19,17 +49,10 @@ export default function () {
               <input type="password" required />
              
             </div>
-            <div className={styles.button}>Entrar</div>
+            
           </form>
-          <div className={styles.linksContainer}>
-            <div className={styles.linksb}>
-              <a href="#">Esqueceu a senha? Redefina</a>
-            </div>
-            <div className={styles.links}>
-              <a href="#">Não tem conta? Crie uma agora</a>
-            </div>
-          </div> */}
-        </div>
+          */}
+        {/* </div> */}
       </div>
     </>
   )
